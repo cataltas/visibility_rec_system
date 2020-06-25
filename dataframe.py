2db8fe8e2d0a77fa26631b34dfd6bb9219d0c1ee
Camille Taltas <ctaltas@nd.edu>
2020-06-25 17:27:31 +0100

    part 2

---
+++ dataframe.py
@@ -0,0 +1,37 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys, getopt, random
+import os
+import pandas as pd
+from pyspark.sql import SparkSession
+
+filepath = "hdfs:/user/ct2522"
+
+def newSparkSession():
+    # return SparkSession.builder.getOrCreate()
+    mem = "7GB"
+    spark = (SparkSession.builder.appName("Music_Project")
+             .master("yarn")
+             .config("sparn.executor.memory", mem)
+             .config("sparn.driver.memory", mem)
+             .getOrCreate())
+    spark.sparkContext.setLogLevel("ERROR")
+    return spark
+
+def get_ids(file_path,sparkSession=None):
+    spark = sparkSession or newSparkSession()
+    gender = spark.read.parquet("{}/{}".format(filepath, "gender_df.parquet"))
+    gender.createOrReplaceTempView("gender")
+    cid ="36b35ee75fec40c399220f9371d2e3b0" 
+    secret = "c0ce447c51394e1198dc56fb787ee326"
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    for i in gender["name"]:
+        print(i)
+def main():
+    get_ids(filepath)    
+
+if __name__ == "__main__":
+    main()
+
+
