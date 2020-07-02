84ba01a46ac7db713a9ff2e654de850886b7fc2c
Camille Taltas <ctaltas@nd.edu>
2020-07-02 12:48:25 +0100

    last extract

--- popularity.py
+++ popularity.py
@@ -28,34 +28,30 @@ def get_ids(file_path,sparkSession=None):
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     gender_df = gender.select("*").toPandas()
     gender_df=pd.DataFrame(gender_df,columns=["name","gender"])
-    id_df = pd.DataFrame()
-    for i,artist in enumerate(gender_df["name"]):
-        try:
-            artist_search = sp.search(q=artist, type='artist', limit=50,offset=0)
-        except:
-            print ("Timeout occurred")
-        for info in artist_search['artists']['items']:
-            if artist in info["name"]:
-                temp_id=pd.DataFrame([[artist,gender_df["gender"].iloc[i],info["id"],info["popularity"]]],columns=["Artist","Gender","SongId","Popularity"])
-                id_df = id_df.append(temp_id,ignore_index=True)
-        print(i)
-    df_id=spark.createDataFrame(id_df)  
-    df_id.write.parquet("{}/{}".format(filepath, "id_df.parquet"))
-
-def concat_files(file_path,sparkSession=None):
-    spark = sparkSession or newSparkSession()
-    final_id = spark.read.parquet("{}/{}".format(filepath, "id_df.parquet"))
-    final_id.createOrReplaceTempView("final_id")
-    for i in range(2,7):
-        temp_id = spark.read.parquet("{}/id_df_{}.parquet".format(filepath, i))
-        temp_id.createOrReplaceTempView("temp_id")
-        final_id = final_id.union(temp_id)
-    final_id.write.parquet("{}/{}".format(filepath, "final_id_df.parquet"))
-
+    search = sp.search(q=artist, type='track', limit=50,offset=0)
+    print(search)
+    # id_df = pd.DataFrame()
+    # for i,artist in enumerate(gender_df["name"]):
+    #     try:
+    #         artist_search = sp.search(q=artist, type='track', limit=50,offset=0)
+    #     except:
+    #         print ("Timeout occurred")
+    #     for info in artist_search['artists']['items']:
+    #         if artist in info["name"]:
+    #             temp_id=pd.DataFrame([[artist,gender_df["gender"].iloc[i],info["id"],info["popularity"]]],columns=["Artist","Gender","SongId","Popularity"])
+    #             id_df = id_df.append(temp_id,ignore_index=True)
+    #     print(i)
+    # df_id=spark.createDataFrame(id_df)  
+    # df_id.write.parquet("{}/{}".format(filepath, "id_df.parquet"))
 def main():
     get_ids(filepath)    
 
 if __name__ == "__main__":
     main()
+
+# Total: 321 324 artists, 69 825 females, 251 499 males
+# Total songs: 1 335 517 songs, 250 050 females, 1 085 467 males
+# Songs >=80: 1 576 total, 269 females, 1307 males
+# Songs <10: 920 487 total, 166 823 females, 753 664 males
 
 
