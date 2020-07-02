269a55717b4b79dd871fe1bec350e1f0cc2773c9
Camille Taltas <ctaltas@nd.edu>
2020-07-02 12:27:21 +0100

    last extract

---
+++ songinfo.py
@@ -0,0 +1,43 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys, getopt, random
+import os
+import pandas as pd
+from pyspark.sql import SparkSession
+
+filepath = "hdfs:/user/ct2522"
+
+def newSparkSession():
+    # return SparkSession.builder.getOrCreate()
+    mem = "15GB"
+    spark = (SparkSession.builder.appName("Music_Project")
+             .master("yarn")
+             .config("sparn.executor.memory", mem)
+             .config("sparn.driver.memory", mem)
+             .getOrCreate())
+    spark.sparkContext.setLogLevel("ERROR")
+    return spark
+
+def song_info(file_path,sparkSession=None):
+    spark = sparkSession or newSparkSession()
+    id_df = spark.read.parquet("{}/{}".format(filepath, "id_df.parquet"))
+    id_df.createOrReplaceTempView("id_df")
+    cid ="36b35ee75fec40c399220f9371d2e3b0" 
+    secret = "c0ce447c51394e1198dc56fb787ee326"
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    id_df = id_df.select("*").toPandas()
+    id_df=pd.DataFrame(id_df)
+    # final_df = pd.DataFrame()
+    for i,val in enumerate(id_df["SongId"][0:10]):
+        print(sp.track(val))
+        
+    # df_final=spark.createDataFrame(final_df)  
+    # df_final.write.parquet("{}/{}".format(filepath, "final_music.parquet"))
+
+
+def main():
+    song_info(filepath)    
+
+if __name__ == "__main__":
+    main()
