c72da51d5963d8bd68176f7624346a871133c988
Camille Taltas <ctaltas@nd.edu>
2020-07-04 14:06:59 +0100

    last extract

--- popularity.py
+++ popularity.py
@@ -35,9 +35,8 @@ def get_ids(file_path,sparkSession=None):
             if gender_df["name"].iloc[0] in j["name"]:
                 count+=1
         if count>0:
-            info_list = [gender_df["name"].iloc[0],gender_df["gender"],t["id"],t["popularity"]]
+            info_list = [gender_df["name"].iloc[0],gender_df["gender"].iloc[0],t["id"],t["popularity"]]
             date = t["album"]['release_date']
-            date_prec = t["album"]['release_date_precision']
             date=pd.to_datetime(date).to_period('y')
             info_list.append(date.year)
         print(info_list)
