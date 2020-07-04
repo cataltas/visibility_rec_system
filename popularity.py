b8f79530ac2ecc16c6a5c5122a114b996e3ac9b8
Camille Taltas <ctaltas@nd.edu>
2020-07-04 10:58:04 +0100

    last extract

--- popularity.py
+++ popularity.py
@@ -29,7 +29,9 @@ def get_ids(file_path,sparkSession=None):
     gender_df = gender.select("*").toPandas()
     gender_df=pd.DataFrame(gender_df,columns=["name","gender"])
     search = sp.search(q=gender_df["name"].iloc[0], type='track', limit=2,offset=0)
-    print(search["tracks"]["items"])
+    for i,t in enumerate(search["tracks"]["items"]):
+        print(t["album"])
+    # print(search["tracks"]["items"])
     # id_df = pd.DataFrame()
     # for i,artist in enumerate(gender_df["name"]):
     #     try:
