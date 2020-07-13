75bb6a429b37b64b4cab787a7ef5d3bca52f2c70
Camille Taltas <ctaltas@nd.edu>
2020-07-13 19:18:27 +0100

    fix

--- songinfo.py
+++ songinfo.py
@@ -9,7 +9,7 @@ filepath = "hdfs:/user/ct2522"
 
 def newSparkSession():
     # return SparkSession.builder.getOrCreate()
-    mem = "15GB"
+    mem = "10GB"
     spark = (SparkSession.builder.appName("Music_Project")
              .master("yarn")
              .config("sparn.executor.memory", mem)
