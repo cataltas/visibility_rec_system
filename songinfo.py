0c822f5f3f6a759f285265f62f05701d940a6067
Camille Taltas <ctaltas@nd.edu>
2020-07-18 13:52:36 +0100

    fix

--- songinfo.py
+++ songinfo.py
@@ -20,15 +20,15 @@ def newSparkSession():
 
 def song_info(file_path,sparkSession=None):
     spark = sparkSession or newSparkSession()
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
     id_df = spark.read.parquet("{}/{}".format(filepath, "id_data.parquet"))
     id_df.createOrReplaceTempView("id_df")
     cid ="36b35ee75fec40c399220f9371d2e3b0" 
     secret = "c0ce447c51394e1198dc56fb787ee326"
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    id_df = id_df.select("*")
-    # .toPandas()
-    id_df=pd.DataFrame(id_df)
+    id_df = id_df.select("*").toPandas()
+    # id_df=pd.DataFrame(id_df)
     # final_df = pd.DataFrame()
     # for i,val in enumerate(id_df["SongId"].iloc[0:10]):
     #     print(val)
