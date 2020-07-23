dba55f74ac100f48bdaee5971a43f64017a7932d
Camille Taltas <ctaltas@nd.edu>
2020-07-23 11:30:53 +0200

    hi

--- songinfo.py
+++ songinfo.py
@@ -19,22 +19,23 @@ def newSparkSession():
 
 def song_info(file_path,sparkSession=None):
     spark = sparkSession or newSparkSession()
-    id_df = spark.read.parquet("{}/{}".format(filepath, "id_data_5.parquet"))
-    id_df.createOrReplaceTempView("id_df")
-    cid ="36b35ee75fec40c399220f9371d2e3b0" 
-    secret = "c0ce447c51394e1198dc56fb787ee326"
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    names = id_df.select("SongID").toPandas()
-    print(names.iloc[0:10])
-    # names = names.select("*").toPandas()
-    # final_df = pd.DataFrame()
-    # print(n.iloc[0:10])
-    # for i,val in enumerate(names.iloc[0:50]):
-    #     print(val)
-        
-    # df_final=spark.createDataFrame(final_df)  
-    # df_final.write.parquet("{}/{}".format(filepath, "final_music.parquet"))
+    for i in range(1,7):
+        id_df = spark.read.parquet("{}/{}".format(filepath, "id_data_{}.parquet".format(i)))
+        id_df.createOrReplaceTempView("id_df")
+        cid ="36b35ee75fec40c399220f9371d2e3b0" 
+        secret = "c0ce447c51394e1198dc56fb787ee326"
+        client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+        names = id_df.select("SongID").toPandas()
+        print(names.iloc[0:10])
+        # names = names.select("*").toPandas()
+        # final_df = pd.DataFrame()
+        # print(n.iloc[0:10])
+        # for i,val in enumerate(names.iloc[0:50]):
+        #     print(val)
+            
+        # df_final=spark.createDataFrame(final_df)  
+        # df_final.write.parquet("{}/{}".format(filepath, "final_music.parquet"))
 
 def main():
     song_info(filepath)    
