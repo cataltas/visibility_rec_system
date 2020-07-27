7e9afb5495a4f30d7e6cabf46ebe674ca13162ca
Camille Taltas <ctaltas@nd.edu>
2020-07-27 15:32:55 +0200

    hi

--- songinfo.py
+++ songinfo.py
@@ -38,17 +38,17 @@ def song_info(file_path,sparkSession=None):
             song_ids = names.iloc[i:m]["SongID"].tolist()
         i+=50
         info = sp.audio_features(song_ids)
-        if info != None: 
-            for j,val in enumerate(info):
+        for j,val in enumerate(info):
+            if val!=None:
                 print(val,song_ids[j])
                 info_line = [
                     # song_ids[j],val["danceability"],val["energy"],val["key"],val["loudness"],
                 val["mode"],
                             val["speechiness"],val["acousticness"],val["instrumentalness"],val["liveness"],val["valence"],val["tempo"]]
                 print(info_line)
-    #         temp_info = pd.DataFrame([info_line],columns=["SongID","danceability","energy","key","loudness","mode","speechiness","acousticness",
-    #                                                         "instrumentalness","liveness","valence","tempo"])
-    #         final_df =final_df.append(temp_info,ignore_index = True)
+        #         temp_info = pd.DataFrame([info_line],columns=["SongID","danceability","energy","key","loudness","mode","speechiness","acousticness",
+        #                                                         "instrumentalness","liveness","valence","tempo"])
+        #         final_df =final_df.append(temp_info,ignore_index = True)
         print(i)
     # df_final=spark.createDataFrame(final_df)  
     # df_final.write.parquet("{}/{}".format(filepath, "final_music_1.parquet"))
