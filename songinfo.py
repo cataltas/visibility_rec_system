0931427a2698ad90e0af0a096ef3c3b71a727c79
Camille Taltas <ctaltas@nd.edu>
2020-08-03 11:52:53 +0200

    final

--- songinfo.py
+++ songinfo.py
@@ -28,8 +28,8 @@ def song_info(file_path,sparkSession=None):
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     names = id_df.select("SongID").toPandas()
-    i=0
-    m=int(np.floor(len(names)/2))
+    i=int(np.floor(len(names)/2))
+    m=len(names)
     while i<m:
         if (i+50)<m:
             song_ids = names.iloc[i:i+50]["SongID"].tolist()
@@ -49,7 +49,7 @@ def song_info(file_path,sparkSession=None):
                 final_df =final_df.append(temp_info,ignore_index = True)
         print(i)
     df_final=spark.createDataFrame(final_df)  
-    df_final.write.parquet("{}/{}".format(filepath, "final_music_1.parquet"))
+    df_final.write.parquet("{}/{}".format(filepath, "final_music_2.parquet"))
 
 def concat(file_path,sparkSession=None):
     spark = sparkSession or newSparkSession()
