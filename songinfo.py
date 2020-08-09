a32460fa3f98c9fd22c04a063caca9fdfc0b551d
Camille Taltas <ctaltas@nd.edu>
2020-08-09 03:41:31 +0200

    final

--- songinfo.py
+++ songinfo.py
@@ -29,7 +29,7 @@ def song_info(file_path,sparkSession=None):
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     names = id_df.select("SongID").toPandas()
     i=100000
-    m = 200000
+    m = 150000
     # m=len(names)
     while i<m:
         if (i+50)<m:
