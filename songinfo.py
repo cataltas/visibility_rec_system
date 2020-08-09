016ad09c7464e68bbbd6463217c5340e28799bed
Camille Taltas <ctaltas@nd.edu>
2020-08-09 13:11:19 +0200

    final

--- songinfo.py
+++ songinfo.py
@@ -28,7 +28,7 @@ def song_info(file_path,sparkSession=None):
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     names = id_df.select("SongID").toPandas()
-    i=106600
+    i=106650
     m=107000
     # m=len(names)
     while i<m:
