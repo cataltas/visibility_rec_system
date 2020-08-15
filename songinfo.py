6df433e35c11bee2c4ad48c029e7bfc00e85d5ff
Camille Taltas <ctaltas@nd.edu>
2020-08-15 03:00:52 +0200

    final

--- songinfo.py
+++ songinfo.py
@@ -44,15 +44,19 @@ def song_info(file_path,sparkSession=None):
             if val!=None:
                 info_line = [
                     song_ids[j],
-                    # val["danceability"],
-                    val["energy"],val["key"],val["loudness"],val["mode"],
+                    val["danceability"],
+                    val["energy"],
+                    # val["key"],
+                    val["loudness"],val["mode"],
                             val["speechiness"],val["acousticness"],
                             val["instrumentalness"],
                             val["liveness"],val["valence"],val["tempo"]]
                 temp_info = pd.DataFrame([info_line],columns=[
                     "SongID",
-                # "danceability",
-                "energy","key","loudness","mode","speechiness","acousticness",
+                "danceability",
+                "energy",
+                # "key",
+                "loudness","mode","speechiness","acousticness",
                                                                 "instrumentalness",
                                                                 "liveness","valence","tempo"])
                 print(temp_info)
