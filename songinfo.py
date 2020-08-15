9249503e9f94762bdf9a3de713f102a1d8613c1d
Camille Taltas <ctaltas@nd.edu>
2020-08-15 17:47:14 +0200

    final

--- songinfo.py
+++ songinfo.py
@@ -44,7 +44,7 @@ def song_info(file_path,sparkSession=None):
             if val!=None:
                 info_line = [song_ids[j],val["danceability"],val["energy"],val["key"],val["loudness"],val["mode"],
                             val["speechiness"],val["acousticness"],val["instrumentalness"],val["liveness"],val["valence"],val["tempo"]]
-                info_line[info_line==None]=np.nan 
+                print(info_line[info_line==None])
                 temp_info = pd.DataFrame([info_line],columns=[
                     "SongID","danceability","energy","key","loudness","mode","speechiness",
                 "acousticness","instrumentalness","liveness","valence","tempo"])
